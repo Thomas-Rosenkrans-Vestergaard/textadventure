eab04f382cf096828d8eb0b7cddf051c92d8538a
AlbertVandel <albertvandel@yahoo.dk>
2017-10-26 10:17:01 +0200

    weapon and healing

---
+++ src/main/java/textadventure/items/Healing.java
@@ -0,0 +1,10 @@
+package textadventure.items;
+
+import textadventure.items.medkit.BandAid;
+import textadventure.items.medkit.Gauze;
+import textadventure.items.medkit.MedKit;
+
+public interface Healing
+{
+
+}
---
+++ src/main/java/textadventure/items/Weapon.java
@@ -0,0 +1,12 @@
+package textadventure.items;
+
+public interface Weapon
+{
+
+	/**
+	 * Returns the amount of damage done by the {@link Weapon}.
+	 *
+	 * @return The amount of damage done by the {@link Weapon}.
+	 */
+	int getDamage();
+}
