cd70d5257022f72deb861205cb2a1b74d34dd0a1
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2017-10-27 12:05:09 +0200

    added BaseDoorTest and BaseOptionTest

--- src/test/java/textadventure/doors/BaseDoorTest.java
+++ src/test/java/textadventure/doors/BaseDoorTest.java
@@ -2,6 +2,8 @@ package textadventure.doors;
 
 import org.junit.Test;
 import textadventure.lock.Lock;
+import textadventure.rooms.MockRoom;
+import textadventure.rooms.Room;
 
 import static org.junit.Assert.*;
 
@@ -9,40 +11,109 @@ public class BaseDoorTest
 {
 
 	@Test
-	public void getState() throws Exception
-	{
-
-	}
-
-	@Test
-	public void getLock() throws Exception
-	{
-
-	}
-
-	@Test
-	public void getInverseRoom() throws Exception
-	{
-	}
-
-	@Test
 	public void open() throws Exception
 	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new UnlockedLock();
+		Door door = new BaseDoor(Door.State.CLOSED, lock, a, b);
+		door.open();
+	}
+
+	@Test(expected = DoorAlreadyOpenException.class)
+	public void openDoorAlreadyOpenException() throws Exception
+	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new UnlockedLock();
+		Door door = new BaseDoor(Door.State.OPEN, lock, a, b);
+		door.open();
+	}
+
+	@Test(expected = DoorLockedException.class)
+	public void openDoorLockedException() throws Exception
+	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new LockedLock();
+		Door door = new BaseDoor(Door.State.CLOSED, lock, a, b);
+		door.open();
 	}
 
 	@Test
 	public void close() throws Exception
 	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new UnlockedLock();
+		Door door = new BaseDoor(Door.State.OPEN, lock, a, b);
+		door.close();
+	}
+
+	@Test(expected = DoorAlreadyClosedException.class)
+	public void closeDoorAlreadyClosedException() throws Exception
+	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new UnlockedLock();
+		Door door = new BaseDoor(Door.State.CLOSED, lock, a, b);
+		door.close();
+	}
+
+	@Test(expected = DoorLockedException.class)
+	public void closeDoorLockedException() throws Exception
+	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new LockedLock();
+		Door door = new BaseDoor(Door.State.OPEN, lock, a, b);
+		door.close();
 	}
 
 	@Test
-	public void enter() throws Exception
+	public void getState() throws Exception
 	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new LockedLock();
+		assertEquals(Door.State.OPEN, new BaseDoor(Door.State.OPEN, lock, a, b).getState());
+		assertEquals(Door.State.CLOSED, new BaseDoor(Door.State.CLOSED, lock, a, b).getState());
 	}
 
 	@Test
-	public void inspect() throws Exception
+	public void getLock() throws Exception
 	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new LockedLock();
+		Door door = new BaseDoor(Door.State.CLOSED, lock, a, b);
+		assertSame(lock, door.getLock());
 	}
 
+	@Test
+	public void getInverseRoom() throws Exception
+	{
+		Room a    = new MockRoom();
+		Room b    = new MockRoom();
+		Lock lock = new LockedLock();
+		Door door = new BaseDoor(Door.State.CLOSED, lock, a, b);
+		assertSame(a, door.getInverseRoom(b));
+		assertSame(b, door.getInverseRoom(a));
+	}
+
+	class LockedLock extends Lock
+	{
+		LockedLock()
+		{
+			super("MockLockCode", State.LOCKED);
+		}
+	}
+
+	class UnlockedLock extends Lock
+	{
+		UnlockedLock()
+		{
+			super("MockLockCode", State.UNLOCKED);
+		}
+	}
 }
